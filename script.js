ddaf66fcdf7b6a5725eec2215403180257e9acb7
Irfan Ullah <iu783582>
2024-11-17 18:43:11 +0500

    Time and date

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let dateContainer = document.querySelector(".date-container");
 let hoursContainer = document.querySelector(".hours");
 let minutesContainer = document.querySelector(".minutes");
 let secondsContainer = document.querySelector(".seconds");
+let ampmContainer = document.querySelector(".ampm");
 
-
-const weekdays =[
+const weekdays = [
     "Sunday",
     "Monday",
     "Tuesday",
-    "Wendesday",
+    "Wednesday",
     "Thursday",
     "Friday",
     "Saturday"
@@ -29,35 +29,30 @@ const monthNames = [
     "December"
 ];
 
-
-function formatTime(time){
-    return time < 10 ? "0" + time : time;  //  0 : 45 : 10  -> 00 : 45 : 10 
+function formatTime(time) {
+    return time < 10 ? "0" + time : time;
 }
 
-function updateClock(){
-
-
+function updateClock() {
     const today = new Date();
-   // console.log(today);
     let date = today.getDate();
-   // console.log(date)
     let day = weekdays[today.getDay()];
     let month = monthNames[today.getMonth()];
 
-    // console.log(day,month)
-    // console.log(today.getDay(),today.getMonth());
-
-    let hours = formatTime(today.getHours());
+    let hours = today.getHours();
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
     let minutes = formatTime(today.getMinutes());
     let seconds = formatTime(today.getSeconds());
 
+    dateContainer.innerHTML = `<p>${day}</p><p><span>${date}</span></p><p>${month}</p>`;   
 
-     dateContainer.innerHTML = ` <p>${day}</p><p><span>${date}</span></p><p>${month}</p>`;   
-
-     hoursContainer.textContent = hours + ":";
-     minutesContainer.textContent = minutes + ":";
-     secondsContainer.textContent = seconds;
-
+    hoursContainer.textContent = formatTime(hours);
+    minutesContainer.textContent = ":" + minutes;
+    secondsContainer.textContent = ":" + seconds;
+    ampmContainer.textContent = ampm;
 }
 
-setInterval(updateClock,1000);
+setInterval(updateClock, 1000);
+updateClock(); // Call once immediately to avoid delay
